b5cd5c415241ee909be023295260404160418d7c
Brian Levin <bml201095@gmail.com>
2020-08-12 21:44:16 -0400

    added a preotype to get the htp request using a call back function

--- easyhttp.js
+++ easyhttp.js
@@ -2,3 +2,9 @@ function easyHTTP() {
     this.http = new XMLHttpRequest();
   }
   
+  // Make an HTTP GET Request
+easyHTTP.prototype.get = function(url, callback) {
+    this.http.open('GET', url, true);
+
+
+}
