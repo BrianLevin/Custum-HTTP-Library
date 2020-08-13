47309694e05f6b4b207421afee7945aa03d64fc0
Brian Levin <bml201095@gmail.com>
2020-08-12 23:59:02 -0400

    rearranged files

--- app.js
+++ app.js
@@ -1,16 +1,3 @@
-//const http = new easyHTTP;
-
-// Get Posts
-//http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
-  //if(err) {
-    //console.log(err);
-  //} else {
-    //console.log(posts);
-  //}
-//});
-
-
-
 
 const http = new easyHTTP;
 
@@ -38,6 +25,25 @@ const data = {
   body: 'This is a custom post'
 };
 
+// Create Post
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Update Post
+// http.put('https://jsonplaceholder.typicode.com/posts/5', data, function(err, post) {
+//   if(err) {
+//     console.log(err);
+//   } else {
+//     console.log(post);
+//   }
+// });
+
+// Delete Post
 http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
   if(err) {
     console.log(err);
